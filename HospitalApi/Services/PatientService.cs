46566604b625d19ccf5ffa798f81c6f9d5604e57
MarcelKrawczyk <marcelkrawczyk04@gmail.com>
2026-06-06 23:28:41 +0200

    Implements GetAllPatientsAsync method with optional search filtering

--- HospitalApi/Services/PatientService.cs
+++ HospitalApi/Services/PatientService.cs
@@ -9,9 +9,70 @@ namespace HospitalApi.Services;
 public class PatientService(HospitalDbContext db)
 {
     public async Task<IEnumerable<PatientDto>> GetAllPatientsAsync(string? search)
+{
+    var query = db.Patients
+        .Include(p => p.Admissions).ThenInclude(a => a.Ward)
+        .Include(p => p.BedAssignments).ThenInclude(ba => ba.Bed).ThenInclude(b => b.BedType)
+        .Include(p => p.BedAssignments).ThenInclude(ba => ba.Bed).ThenInclude(b => b.Room).ThenInclude(r => r.Ward)
+        .AsQueryable();
+
+    if (!string.IsNullOrWhiteSpace(search))
     {
-        throw new NotImplementedException();
+        query = query.Where(p =>
+            EF.Functions.Like(p.FirstName, $"%{search}%") ||
+            EF.Functions.Like(p.LastName, $"%{search}%"));
     }
+
+    var patients = await query.ToListAsync();
+
+    return patients.Select(p => new PatientDto
+    {
+        Pesel = p.Pesel,
+        FirstName = p.FirstName,
+        LastName = p.LastName,
+        Age = p.Age,
+        Sex = p.Sex ? "Male" : "Female",
+        Admissions = p.Admissions.Select(a => new AdmissionDto
+        {
+            Id = a.Id,
+            AdmissionDate = a.AdmissionDate,
+            DischargeDate = a.DischargeDate,
+            Ward = new WardDto
+            {
+                Id = a.Ward.Id,
+                Name = a.Ward.Name,
+                Description = a.Ward.Description
+            }
+        }).ToList(),
+        BedAssignments = p.BedAssignments.Select(ba => new BedAssignmentDto
+        {
+            Id = ba.Id,
+            From = ba.From,
+            To = ba.To,
+            Bed = new BedDto
+            {
+                Id = ba.Bed.Id,
+                BedType = new BedTypeDto
+                {
+                    Id = ba.Bed.BedType.Id,
+                    Name = ba.Bed.BedType.Name,
+                    Description = ba.Bed.BedType.Description
+                },
+                Room = new RoomDto
+                {
+                    Id = ba.Bed.Room.Id,
+                    HasTv = ba.Bed.Room.HasTv,
+                    Ward = new WardDto
+                    {
+                        Id = ba.Bed.Room.Ward.Id,
+                        Name = ba.Bed.Room.Ward.Name,
+                        Description = ba.Bed.Room.Ward.Description
+                    }
+                }
+            }
+        }).ToList()
+    });
+}
 
     public async Task<(BedAssignmentDto? Result, string? Error)> AssignBedAsync(string pesel,
         AssignBedRequestDto request)
